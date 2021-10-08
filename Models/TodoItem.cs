8abe35b8199c61c94f0d64ab103c57fbebffd9aa
Nick Fletcher <nick.fletcher@iria.com.au>
2021-10-09 08:35:26 +1100

    Add binding for listed Todo items.

---
+++ Models/TodoItem.cs
@@ -0,0 +1,10 @@
+namespace BlazorApp.Models
+{
+    public class TodoItem
+    {
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+        
+                
+    }
+}
